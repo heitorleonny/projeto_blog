76c1cfd1888a5a7305b406203977bec7148b2a27
heitorleonny <heitorleonnyyt1234@gmail.com>
2023-12-06 23:58:27 -0300

    corrigindo erros

--- blog/settings.py
+++ blog/settings.py
@@ -142,9 +142,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-# Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
-# ...
